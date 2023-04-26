22fbb406b06112c99e24d212230e9185b869293e
tipstonga <pablo.balonga@gmail.com>
2023-04-26 20:43:01 -0300

    04 ojo plt show

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 import requests
 import numpy as numpy
 import os
-
+import matplotlib
+import matplotlib.pyplot as plt
 
 ## seleccion del host y port para 
 ## Railway o local
@@ -21,11 +22,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+## trabajo generar y salvar grafico
+    print('genero grafico')
+    plt.plot([0, 1, 2, 3, 4], [0, 3, 5, 9, 11])
+    plt.xlabel('Months')
+    plt.ylabel('Books Read')
+    print('salvo grafico sin mostrar')
+    plt.show()
+    # plt.savefig('http://127.0.0.1:8000/figUno.png')
+    # plt.savefig('templates/figUno.png')
+    # plt.savefig('templates/figUno.jpg')
 
+
+## trabajo basico web videos
     datosObtenidos=requests.get('https://api.dailymotion.com/videos?channel=sport&limit=10')
     datosFormatoJSON=datosObtenidos.json()
     print(datosFormatoJSON)
-    print("version 03 -----------")
+    print("version 04 -----------")
 
     return render_template('index.html',datos=datosFormatoJSON['list'])
 
